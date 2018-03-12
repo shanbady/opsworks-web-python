b1e0c41fa4f9684b77c1389b0ed00ebec9dcbf15
Shankar Ambady <shanbady@gmail.com>
2018-03-12 12:58:08 -0400

    adding custom release info command

--- release_info/recipes/default.rb
+++ release_info/recipes/default.rb
@@ -1,10 +1,13 @@
-bash "set_release" do
+branch = node[:deploy][app_name]
+
+script "set_release" do
+  interpreter "bash"
   user "root"
   cwd "/tmp"
   code <<-EOH
-    export DEPLOY_TARGET=$(sudo opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g")
-    sudo opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g"
-    echo "test" > ~/test
-    echo $DEPLOY_TARGET > /home/deploy/latest_release
+    export DEPLOY_TARGET=$(opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g")
+    opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g"
+    echo "test" >> ~/test
+    echo $DEPLOY_TARGET >> /home/deploy/latest_release
   EOH
 end
