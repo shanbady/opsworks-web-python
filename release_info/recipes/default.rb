26a08f33381670ed8c85a02abaac1973808c86b6
Shankar Ambady <shanbady@gmail.com>
2018-03-12 13:43:12 -0400

    adding debug logging

--- release_info/recipes/default.rb
+++ release_info/recipes/default.rb
@@ -8,7 +8,6 @@ script "set_release" do
   code <<-EOH
     export DEPLOY_TARGET=#{revision}
     echo "test"
-    opsworks-agent-cli get_json | grep    "\\"revision\\":" | sed -E "s/\\"revision\\"\\: \\"(.*?)\\",/\\1/" | head -n 1 | sed "s/ //g")
     echo $DEPLOY_TARGET >> /home/deploy/latest_release
   EOH
 end
