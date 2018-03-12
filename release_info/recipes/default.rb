d07c9f50f05a01c87b3b7e523a34c641a4c02c0d
Shankar Ambady <shanbady@gmail.com>
2018-03-12 12:24:57 -0400

    adding custom release info command

--- release_info/recipes/default.rb
+++ release_info/recipes/default.rb
@@ -2,7 +2,9 @@ bash "set_release" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-    export DEPLOY_TARGET=$(echo "" | sudo -S opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g")
+    export DEPLOY_TARGET=$(sudo opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g")
+    sudo opsworks-agent-cli get_json | grep    "\"revision\":" | sed -E "s/\"revision\"\: \"(.*?)\",/\1/" | head -n 1 | sed "s/ //g"
+    echo "test"
     echo $DEPLOY_TARGET > /home/deploy/latest_release
   EOH
 end
