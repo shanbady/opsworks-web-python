6aaf68f7d5171161e7413af3ea4c853f91d8c9a8
Shankar Ambady <shanbady@gmail.com>
2018-03-12 14:03:38 -0400

    fixing environment var

--- release_info/recipes/default.rb
+++ release_info/recipes/default.rb
@@ -1,10 +1,12 @@
 require 'chef/log'
 Chef::Log.level = :debug
 revision = node["deploy"]["mc"]["scm"]["revision"]
+impact_environment = node['deploy']['mc']['environment']['IMPACT_ENVIRONMENT']
 script "set_release" do
   interpreter "bash"
   user "deploy"
   cwd "/srv/www/mc/current"
+  environment node['deploy']['mc']['environment']
   code <<-EOH
     export DEPLOY_TARGET=#{revision}
     make deploy IMAGE_TAG=$DEPLOY_TARGET
