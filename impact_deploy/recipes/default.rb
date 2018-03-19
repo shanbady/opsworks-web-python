6cf693c9ef26967e3d2dfb4fd663d7429cbca942
Shankar Ambady <shanbady@gmail.com>
2018-03-19 13:20:09 -0400

    circumventing make deploy command

--- impact_deploy/recipes/default.rb
+++ impact_deploy/recipes/default.rb
@@ -2,6 +2,9 @@ require 'chef/log'
 Chef::Log.level = :debug
 revision = node["deploy"]["mc"]["scm"]["revision"]
 impact_environment = node['deploy']['mc']['environment']['IMPACT_ENVIRONMENT']
+docker_registry = node['deploy']['mc']['environment']['DOCKER_REGISTRY']
+ecs_secret_access_key = node['deploy']['mc']['environment']['ECS_SECRET_ACCESS_KEY']
+ecs_access_key_id = node['deploy']['mc']['environment']['ECS_ACCESS_KEY_ID']
 script "set_release" do
   interpreter "bash"
   user "deploy"
@@ -9,10 +12,16 @@ script "set_release" do
   environment node['deploy']['mc']['environment']
   code <<-EOH
     export DEPLOY_TARGET=#{revision}
+    export IMPACT_ENVIRONMENT=#{impact_environment}
+    export DOCKER_REGISTRY=#{docker_registry}
+    export DOCKER_REGISTRY=#{docker_registry}
+    export ECS_SECRET_ACCESS_KEY=#{ecs_secret_access_key}
+    export ECS_ACCESS_KEY_ID=#{ecs_access_key_id}
     if [ -z "$IMPACT_ENVIRONMENT" ]; then 
     	echo "IMPACT_ENVIRONMENT not set. deploy skipped";
     else
         make deploy IMAGE_TAG=$DEPLOY_TARGET;
+        ecs deploy --ignore-warnings $(IMPACT_ENVIRONMENT) impact --image web $(DOCKER_REGISTRY)/impact-api:$DEPLOY_TARGET --image redis $(DOCKER_REGISTRY)/redis:$DEPLOY_TARGET --access-key-id $(ECS_ACCESS_KEY_ID) --secret-access-key $(ECS_SECRET_ACCESS_KEY) 2>/dev/null
      fi
   EOH
 end
