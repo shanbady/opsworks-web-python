f5ed9df1069d4ae3bf7414843c79925788e6a5c3
Shankar Ambady <shanbady@gmail.com>
2018-03-29 11:23:52 -0400

    testing changes to pip install

--- impact_deploy/recipes/default.rb
+++ impact_deploy/recipes/default.rb
@@ -21,6 +21,7 @@ script "set_release" do
     	echo "IMPACT_ENVIRONMENT not set. deploy skipped";
     else
         virtualenv --no-site-packages .venv && source .venv/bin/activate
+        pip install pyOpenSSL ndg-httpsclient pyasn1
         pip install ecs-deploy
         ecs deploy --ignore-warnings $IMPACT_ENVIRONMENT impact --image web $DOCKER_REGISTRY/impact-api:$DEPLOY_TARGET --image redis $DOCKER_REGISTRY/redis:$DEPLOY_TARGET --access-key-id $ECS_ACCESS_KEY_ID --secret-access-key $ECS_SECRET_ACCESS_KEY 2>/dev/null
      fi
