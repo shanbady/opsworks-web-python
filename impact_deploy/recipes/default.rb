aaa0f1740e630d72be10039f44a16e9a222e4c28
Yotam <yotammanor@gmail.com>
2018-04-08 13:39:21 +0300

    set longer timeout

--- impact_deploy/recipes/default.rb
+++ impact_deploy/recipes/default.rb
@@ -25,7 +25,7 @@ script "set_release" do
         virtualenv --no-site-packages .venv && source .venv/bin/activate
         pip install --upgrade certifi pyopenssl requests[security] ndg-httpsclient pyasn1 pip
         pip install ecs-deploy
-        .venv/local/bin/ecs deploy --ignore-warnings $IMPACT_ENVIRONMENT impact --image web $DOCKER_REGISTRY/impact-api:$DEPLOY_TARGET --image redis $DOCKER_REGISTRY/redis:$DEPLOY_TARGET --access-key-id $ECS_ACCESS_KEY_ID --secret-access-key $ECS_SECRET_ACCESS_KEY --region $AWS_DEFAULT_REGION
+        .venv/local/bin/ecs deploy --ignore-warnings $IMPACT_ENVIRONMENT impact --image web $DOCKER_REGISTRY/impact-api:$DEPLOY_TARGET --image redis $DOCKER_REGISTRY/redis:$DEPLOY_TARGET --access-key-id $ECS_ACCESS_KEY_ID --secret-access-key $ECS_SECRET_ACCESS_KEY --region $AWS_DEFAULT_REGION --timeout 600
      fi
   EOH
 end
