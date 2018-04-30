2b1988964ef32306c46d1361fd8bfb83deb398c0
Shankar Ambady <shanbady@gmail.com>
2018-04-30 09:44:37 -0400

    fixed variable names

--- impact_deploy/recipes/install_keys.rb
+++ impact_deploy/recipes/install_keys.rb
@@ -25,17 +25,17 @@ script "install_keys" do
         pip install awscli --upgrade
         .venv/local/bin/aws s3api get-object --bucket masschallenge-deployment --key secure/ecs-key.pem ~/.ssh/ecs-key.pem
         export ECS_PEM_FILE=~/.ssh/ecs-key.pem
-        export ECS_INSTANCES=`.venv/local/bin/aws ecs list-container-instances --cluster $IMPACT_ENVIRONMENT | grep arn | cut -b 64-99 | xargs`
-        export EC2INSTANCES=`.venv/local/bin/aws ecs describe-container-instances --container-instances $ECS_INSTANCES --cluster $IMPACT_ENVIRONMENT --query "containerInstances[].ec2InstanceId" --output text`
-        export EC2_INSTANCE_IP=`.venv/local/bin/aws ec2 describe-instances --instance-ids $EC2INSTANCES --query "Reservations[].Instances[].PublicIpAddress" --output text`
+        export ECS_CONTAINERS=`.venv/local/bin/aws ecs list-container-instances --cluster $IMPACT_ENVIRONMENT | grep arn | cut -b 64-99 | xargs`
+        export EC2_INSTANCES=`.venv/local/bin/aws ecs describe-container-instances --container-instances $ECS_CONTAINERS --cluster $IMPACT_ENVIRONMENT --query "containerInstances[].ec2InstanceId" --output text`
+        export EC2_INSTANCE_IP=`.venv/local/bin/aws ec2 describe-instances --instance-ids $EC2_INSTANCES --query "Reservations[].Instances[].PublicIpAddress" --output text`
         export DEPLOY_KEY=$(ssh  -o "StrictHostKeyChecking no" -i $ECS_PEM_FILE ec2-user@$EC2_INSTANCE_IP  /bin/cat /home/ec2-user/.ssh/authorized_keys)
         echo "echo $DEPLOY_KEY > /home/ec2-user/.ssh/authorized_keys" | ssh  -o "StrictHostKeyChecking no" -i $ECS_PEM_FILE ec2-user@$EC2_INSTANCE_IP  /bin/bash
         IFS=' ' read  -a users <<< $(aws opsworks --region us-east-1 describe-user-profiles --query "UserProfiles[].SshUsername" --output text)
         for user in ${users[@]}; do
               echo $user
               .venv/local/bin/aws iam list-ssh-public-keys --user-name "${user}" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
-              export SSHKEY=$(.venv/local/bin/aws iam get-ssh-public-key --user-name "${user}" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text)
-              echo "echo $SSHKEY >> /home/ec2-user/.ssh/authorized_keys" | ssh  -o "StrictHostKeyChecking no" -i $ECS_PEM_FILE ec2-user@$EC2_INSTANCE_IP  /bin/bash
+              export SSH_KEY=$(.venv/local/bin/aws iam get-ssh-public-key --user-name "${user}" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text)
+              echo "echo $SSH_KEY >> /home/ec2-user/.ssh/authorized_keys" | ssh  -o "StrictHostKeyChecking no" -i $ECS_PEM_FILE ec2-user@$EC2_INSTANCE_IP  /bin/bash
             done
         done
      fi
