9dd664120a67f485d7a82dd64f5f580f4d63d4fd
Yotam <yotammanor@gmail.com>
2018-05-01 16:57:09 +0300

    change script to symlink to volume

--- impact_deploy/recipes/mount_volume.rb
+++ impact_deploy/recipes/mount_volume.rb
@@ -6,7 +6,18 @@ script "mount_volume" do
   cwd "/home/deploy"
   environment node['deploy']['mc']['environment']
   code <<-EOH
-    mkdir /mnt/www/
-    sudo ln -d /mnt/www/ /srv/www/
+    cd /vol/www/
+    mkdir mc
+    cd /srv/www/
+    if [ -d mc ]; then
+      mv mc mc_bak
+      ln -s /vol/www/mc/ mc
+      mv mc_bak/current mc/current
+      mv mc_bak/releases mc/releases
+      mv mc_bak/shared mc/shared
+      rm -r mc_bak
+    else
+      ln -s /vol/www/mc/ mc
+    fi
   EOH
 end
