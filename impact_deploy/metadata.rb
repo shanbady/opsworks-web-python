7d7e4c193bcfae96f45320d7c6b3ffd795261536
Yotam Manor <yotammanor@gmail.com>
2018-05-02 15:05:47 +0300

    fix incorrect merge conflict resolution

--- impact_deploy/metadata.rb
+++ impact_deploy/metadata.rb
@@ -1,6 +1,2 @@
 name "impact_deploy"
 version "0.1.0"
-
-
-recipe 'impact_deploy', 'deploys a version of the impact api that has the same release tag being deployed to accelerate'
-recipe 'impact_deploy::mount_volume', 'create a symlink to EBS volume'
