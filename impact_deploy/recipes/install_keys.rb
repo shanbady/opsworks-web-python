ff74d028cb5b35055d72868dc6c9f2dbf5ca2967
Shankar Ambady <shanbady@gmail.com>
2018-05-14 18:30:15 -0400

    removing debug output

--- impact_deploy/recipes/install_keys.rb
+++ impact_deploy/recipes/install_keys.rb
@@ -10,7 +10,7 @@ script "install_keys" do
   cwd "/home/deploy"
   environment node['deploy']['mc']['environment']
   code <<-EOC
-    echo "trigger a deploy of impact-api" >> temp.txt
+    echo "trigger a deploy of impact-api"
     export IMPACT_ENVIRONMENT=#{impact_environment}
     export AWS_DEFAULT_REGION=#{ecs_default_region}
     export ECS_SECRET_ACCESS_KEY=#{ecs_secret_access_key}
@@ -18,9 +18,9 @@ script "install_keys" do
     export AWS_ACCESS_KEY_ID=#{ecs_access_key_id}
     export AWS_SECRET_ACCESS_KEY=#{ecs_secret_access_key}
     if [ -z "$IMPACT_ENVIRONMENT" ]; then 
-    	echo "IMPACT_ENVIRONMENT not set. install keys skipped" >> temp.txt;
+    	echo "IMPACT_ENVIRONMENT not set. install keys skipped"
     else
-        echo "trying to install keys" >> temp.txt;
+        echo "trying to install keys"
         virtualenv --no-site-packages .venv && source .venv/bin/activate
         pip install awscli --upgrade
         .venv/local/bin/aws s3api get-object --bucket masschallenge-deployment --key secure/ecs-key.pem ~/.ssh/ecs-key.pem
@@ -33,7 +33,6 @@ script "install_keys" do
         echo "echo $DEPLOY_KEY > /home/ec2-user/.ssh/authorized_keys" | ssh  -o "StrictHostKeyChecking no" -i $ECS_PEM_FILE ec2-user@$EC2_INSTANCE_IP  /bin/bash && \
         IFS=' ' read  -a users <<< $(aws opsworks --region us-east-1 describe-user-profiles --query "UserProfiles[].SshUsername" --output text)
         for user in ${users[@]}; do
-              echo "${user}" >> temp.txt
               .venv/local/bin/aws iam list-ssh-public-keys --user-name "${user}" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
               export SSH_KEY=$(.venv/local/bin/aws iam get-ssh-public-key --user-name "${user}" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text)
               echo "echo $SSH_KEY >> /home/ec2-user/.ssh/authorized_keys" | ssh  -o "StrictHostKeyChecking no" -i $ECS_PEM_FILE ec2-user@$EC2_INSTANCE_IP  /bin/bash && \
