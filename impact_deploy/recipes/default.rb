63916a77bcd227e53953a1f4a4113ac0c0c1ff99
Vincent Asante <vincenthokie@gmail.com>
2018-10-26 18:15:47 +0300

    allow use of custom service names

--- impact_deploy/recipes/default.rb
+++ impact_deploy/recipes/default.rb
@@ -2,6 +2,7 @@ require 'chef/log'
 Chef::Log.level = :debug
 revision = node["deploy"]["mc"]["scm"]["revision"]
 impact_environment = node['deploy']['mc']['environment']['IMPACT_ENVIRONMENT']
+impact_service = node['deploy']['mc']['environment']['IMPACT_SERVICE']
 docker_registry = node['deploy']['mc']['environment']['DOCKER_REGISTRY']
 ecs_secret_access_key = node['deploy']['mc']['environment']['ECS_SECRET_ACCESS_KEY']
 ecs_access_key_id = node['deploy']['mc']['environment']['ECS_ACCESS_KEY_ID']
@@ -15,6 +16,7 @@ script "set_release" do
     # trigger a deploy of impact-api
     export DEPLOY_TARGET=#{revision}
     export IMPACT_ENVIRONMENT=#{impact_environment}
+    export IMPACT_SERVICE=#{impact_service}
     export DOCKER_REGISTRY=#{docker_registry}
     export AWS_DEFAULT_REGION=#{ecs_default_region}
     export DOCKER_REGISTRY=#{docker_registry}
@@ -26,7 +28,7 @@ script "set_release" do
         virtualenv --no-site-packages .venv && source .venv/bin/activate
         pip install --upgrade certifi pyopenssl requests[security] ndg-httpsclient pyasn1 pip
         pip install ecs-deploy
-        .venv/local/bin/ecs deploy --ignore-warnings $IMPACT_ENVIRONMENT impact --image web $DOCKER_REGISTRY/impact-api:$DEPLOY_TARGET --image redis $DOCKER_REGISTRY/redis:$DEPLOY_TARGET --access-key-id $ECS_ACCESS_KEY_ID --secret-access-key $ECS_SECRET_ACCESS_KEY --region $AWS_DEFAULT_REGION --timeout 600
+        .venv/local/bin/ecs deploy --ignore-warnings $IMPACT_ENVIRONMENT $IMPACT_SERVICE --image web $DOCKER_REGISTRY/impact-api:$DEPLOY_TARGET --image redis $DOCKER_REGISTRY/redis:$DEPLOY_TARGET --access-key-id $ECS_ACCESS_KEY_ID --secret-access-key $ECS_SECRET_ACCESS_KEY --region $AWS_DEFAULT_REGION --timeout 600
         rm -rf .venv/
      fi
   EOH
