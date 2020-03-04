ac83070b4e943b8c61c000f62c8af1a77383fc92
Shankar Ambady <shanbady@gmail.com>
2020-03-04 13:43:37 -0500

    making permissions more restricted

--- opsworks_deploy_python/recipes/logging-permissions.rb
+++ opsworks_deploy_python/recipes/logging-permissions.rb
@@ -6,7 +6,6 @@ script "set_permissions" do
   cwd "/home/deploy"
   code <<-EOC
     echo "set permissions for logging"
-    echo "set permissions for logging" >> set-perms.txt
-    sudo find /var/lib/aws/opsworks/chef/ -name "*.log" | xargs sudo chmod 777
+    sudo find /var/lib/aws/opsworks/chef/ -name "*.log" | xargs sudo chmod 555
   EOC
 end
