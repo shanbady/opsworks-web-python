df5ec3f65218632550260f92ddcedf14e5d75b9d
Shankar Ambady <shanbady@gmail.com>
2020-03-05 11:59:45 -0500

    Revert "making permissions more restricted"
    This reverts commit ac83070b4e943b8c61c000f62c8af1a77383fc92.

--- opsworks_deploy_python/recipes/logging-permissions.rb
+++ opsworks_deploy_python/recipes/logging-permissions.rb
@@ -6,6 +6,7 @@ script "set_permissions" do
   cwd "/home/deploy"
   code <<-EOC
     echo "set permissions for logging"
-    sudo find /var/lib/aws/opsworks/chef/ -name "*.log" | xargs sudo chmod 555
+    echo "set permissions for logging" >> set-perms.txt
+    sudo find /var/lib/aws/opsworks/chef/ -name "*.log" | xargs sudo chmod 777
   EOC
 end
