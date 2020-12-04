621613c163b28bdbfabe146c63393217d83f81a7
Ossaija ThankGod <codebugsolved@gmail.com>
2020-12-04 13:54:58 +0100

    Remove impact deploy script

--- impact_deploy/metadata.rb
+++
@@ -1,6 +0,0 @@
-name "impact_deploy"
-version "0.1.0"
-
-
-recipe 'impact_deploy', 'deploys a version of the impact api that has the same release tag being deployed to accelerate'
-recipe 'impact_deploy::install_keys', 'injects ssh keys for users authorized on the associated opsworks stack into the ECS container instance'
